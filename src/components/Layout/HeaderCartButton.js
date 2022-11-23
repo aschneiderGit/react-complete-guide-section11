85b143e332208b4a7d8a9a0ca4f1524d8f77b8c5
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-23 10:26:11 +0100

    animation on Addd Item Cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,31 @@
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import style from './HeaderCartButton.module.css';
 
 function HeaderCartButton(props) {
+	const [btnBump, setBtnBump] = useState(false);
 	const cartCtx = useContext(CartContext);
+	const {items} = cartCtx;
 	const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
 		return curNumber + item.amount;
 	}, 0);
 
+	const btnClasses = `${style.button} ${btnBump ? style.bump : ''}`;
+
+	useEffect(() => {
+		setBtnBump(true);
+		const timer = setTimeout(() => {
+			setBtnBump(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	return (
-		<button className={style.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={style.icon}>
 				<CartIcon />
 			</span>
